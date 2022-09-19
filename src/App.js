cd21cf1dd49f1ddda01a83195aae9de32678df46
Pochyxi <adienerlopez@gmail.com>
2022-09-19 15:43:05 +0200

    login con funzionalità redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LinkedinNavBar from "./component/LinkedinNavBar";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginComponent from "./components/LoginComponent";
 
 function App() {
   return (
     <BrowserRouter>
-      <LinkedinNavBar />
-      <Routes>
-      </Routes>
+      <Container fluid className="p-0">
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
---
+++ src/components/LoginComponent.jsx
@@ -0,0 +1,45 @@
+import { Col, Container, Row } from "react-bootstrap"
+import { FaUserAlt } from 'react-icons/fa'
+import { useDispatch } from "react-redux"
+import { fetchUser } from "../redux/actions/actions"
+
+const LoginComponent = () => {
+    const dispatch = useDispatch()
+    return (
+        <Container fluid>
+            <Row className="d-flex flex-column flex-lg-row justify-content-between">
+                <Col xs={12} lg={6} className='pt-5 ps-4'>
+                    <p className="loginText pt-5">Unisciti alla più grande community professionale</p>
+                    <Row className="pt-5 text-center justify-content-between">
+                        <Col
+                            onClick={() => {
+                                dispatch(fetchUser('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzI4MWFhZjZkNzlhNTAwMTUwOTAyZWIiLCJpYXQiOjE2NjM1NzI2NTUsImV4cCI6MTY2NDc4MjI1NX0.Ds060cYZ_bHxEDSMxED7ORVkIVrU34tLYtwc_4RtJMk'))
+                            }}
+                            xs={3}
+                            className="loginUser">
+                            <FaUserAlt />
+                            <p>Adiener</p>
+                        </Col>
+                        <Col
+                            onClick={() => {
+                                dispatch(fetchUser('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzI4MWE3ZjZkNzlhNTAwMTUwOTAyZWEiLCJpYXQiOjE2NjM1NzI2MDgsImV4cCI6MTY2NDc4MjIwOH0.2hdtXjC--6_rLMvlCzuIUbqOMYhPbOsWfYRrh1XiEis'))
+                            }}
+                            xs={3}
+                            className="loginUser">
+                            <FaUserAlt />
+                            <p>Manuel</p>
+                        </Col>
+                        <Col xs={3} className="loginUser">
+                            <FaUserAlt />
+                            <p>Ruben</p>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col className="loginLogoCol" xs={12} lg={6}>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default LoginComponent
