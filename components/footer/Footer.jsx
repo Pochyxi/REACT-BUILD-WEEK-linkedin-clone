3266c627353ae0f88d423385085d93adfafee2a9
Rbn444 <106075599+Rbn444@users.noreply.github.com>
2022-09-19 18:02:05 +0200

    demo

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+const MyFooter = () => {
+    return (
+        <div className="Footer">
+            <span>LINKEDIN svg</span>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )}
+
+export default MyFooter;
