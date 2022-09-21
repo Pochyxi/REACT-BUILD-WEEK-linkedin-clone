d2dad926948c8714334f7d4db7db3c07409de6c9
Pochyxi <adienerlopez@gmail.com>
2022-09-21 14:41:19 +0200

    adiener: cardProfileHome

--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -40,23 +40,26 @@ const HomeComponent = () => {
 
 
   return (
-    <Container>
+    <Container fluid>
       <Row className="justify-content-center flex-nowrap px-4 pt-3 pb-2">
         <Col xs={3}>
-        <CardProfiloHome/>
+          <CardProfiloHome />
         </Col>
         <Col xs={6}>
-        <CardCreaPost/>
-        {
-          allPosts &&
-          allPosts.map((post, index) => (
-            <CardPost key={index} post={post}/>
-          ))
-        }
+          <CardCreaPost />
+          {
+            allPosts &&
+            allPosts.map((post, index) => (
+              <CardPost key={index} post={post} />
+            ))
+          }
         </Col>
         <Col xs={3}>
-        <CardPubblicità />
+
         </Col>
+      </Row>
+      <Row>
+        <CardPubblicità />
       </Row>
     </Container>
   );
