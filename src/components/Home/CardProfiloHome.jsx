fb40d7792a057a4b8dc104af840412acbd4c742f
Pochyxi <adienerlopez@gmail.com>
2022-09-21 15:28:39 +0200

    add

--- src/components/Home/CardProfiloHome.jsx
+++ src/components/Home/CardProfiloHome.jsx
@@ -5,18 +5,18 @@ import { Col } from 'react-bootstrap'
 const CardProfiloHome = () => {
     const user = useSelector(state => state.user.user)
     return (
-        <Col className='CardProfile'>
-            <Col className="CardProfileImages d-flex justify-content-center" xs={12} >
+        <>
+            <Col className="CardProfileImages d-flex justify-content-center marginBottom" xs={12} >
                 <Col xs={12} className='HomeCardProfileCerchio justify-content-center d-flex align-items-end'>
                     <img className="img-fluid HomeCardProfileCerchioImg" src={user.image} alt="" />
                 </Col>
             </Col>
-            <Col xs={12} className='CardProfileText text-center mt-5'>
+            <Col xs={12} className='CardProfileText text-center mt-5 marginBottom'>
                 <h5>{user.name} {user.surname}</h5>
                 <p>{user.title}</p>
-                <p className="text-secondary">{user.area}</p>
+                <p className="text-secondary d-none d-md-flex">{user.area}</p>
             </Col>
-            <Col xs={12} className='d-flex justify-content-start p-2'>
+            <Col xs={12} className='d-flex justify-content-start p-2 marginBottom d-none d-md-flex'>
                 <Col xs={10}>
                     <p className='text-seconday'>Collegamenti</p>
                     <h6>Espandi la tua rete</h6>
@@ -25,7 +25,8 @@ const CardProfiloHome = () => {
                     <p className='text-primary text-end'>4</p>
                 </Col>
             </Col>
-        </Col>
+        </>
+
     )
 }
 
--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -25,11 +25,11 @@ const HomeComponent = () => {
 
   const deletePostWithoutImage = (arr) => {
     let arrDelete = []
-    for (let i = arr.length - 1 ; i > 1; i--) {
-      if (!typeof arr[i].user === 'object' ) {
-       arrDelete = arr.filter(item => item._id === arr[i]._id)
+    for (let i = arr.length - 1; i > 1; i--) {
+      if (!typeof arr[i].user === 'object') {
+        arrDelete = arr.filter(item => item._id === arr[i]._id)
+      }
     }
-  }
     return arrDelete
   }
 
@@ -57,24 +57,29 @@ const HomeComponent = () => {
 
 
   return (
-    <Container>
-      <Row className="justify-content-center flex-nowrap px-4 pt-3 pb-2">
-        <Col xs={3}>
-        <CardProfiloHome/>
+    <Container className="container-lg-fluid">
+      <Row className="flex-column flex-lg-row justify-content-between flex-wrap px-4 pt-3 pb-2">
+        <Col xs={12} lg={9} className='d-flex flex-column flex-md-row justify-content-between'>
+          <Col xs={12} sm={12} md={3}>
+            <CardProfiloHome />
+          </Col>
+          <Col xs={12} sm={12} md={8}>
+            <CardCreaPost />
+            {
+              allPosts.map((post, i) => (
+                <LinkedinPost key={i} post={post} />
+              ))
+            }
+            {/* <LinkedinPost post={allPosts} /> */}
+          </Col>
         </Col>
-        <Col xs={6}>
-        <CardCreaPost/>
-        {
-          allPosts.map((post, i) => (
-            <LinkedinPost key={i} post={post} />
-          ))
-        }
-                {/* <LinkedinPost post={allPosts} /> */}
-        </Col>
-        <Col xs={3}>
-        <CardPubblicità />
+        <Col>
+          <Col xs={3} >
+            <CardPubblicità />
+          </Col>
         </Col>
       </Row>
+
     </Container>
   );
 };
