0d29111e43ae8010eba063d64449080d49ccd734
ManuelSenatore <106093444+ManuelSenatore@users.noreply.github.com>
2022-09-21 16:15:10 +0200

    post

--- src/components/Home/CardCreaPost.jsx
+++ src/components/Home/CardCreaPost.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import Avatar from "@mui/material/Avatar";
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
+import EventIcon from '@mui/icons-material/Event';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
 
 const CardCreaPost = () => {
   const user = useSelector((state) => state.user.user);
 
   return (
-    <div>
-      <Row xs={10}>
-        <Col xs={10} className="d-flex">
+    <Col className="CardPost mb-3 ">
+        <Col xs={12} className="CardCompetenze mt-3">
+      <Row>
+        <Col  className="d-flex">
           <Avatar>
             <img className="PostUser" src={user.image} alt="user" />
           </Avatar>
@@ -18,10 +23,22 @@ const CardCreaPost = () => {
           </Button>
         </Col>
       </Row>
-      <Row>
-        <Col></Col>
+      <Row className="rigaIconePost mt-3">
+        <Col className="d-flex no-wrap">
+        <ImageOutlinedIcon color="primary" /> <span className="text-secondary">Foto</span>
+        </Col>
+        <Col className="d-flex no-wrap">
+        <SmartDisplayIcon color="success" /> <span className="text-secondary">Video</span>
+        </Col>
+        <Col className="d-flex no-wrap">
+        <EventIcon sx={{ color: 'orange' }} /> <span className="text-secondary">Evento</span>
+        </Col>
+        <Col className="d-flex no-wrap">
+        <NewspaperIcon sx={{ color: 'orangered' }} /> <span className="text-secondary">Scrivi un articolo</span>
+        </Col>
       </Row>
-    </div>
+    </Col>
+    </Col>
   );
 };
 
