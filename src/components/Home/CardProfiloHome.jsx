d3b229ce91184908d890d256af80cd17614db78b
Pochyxi <adienerlopez@gmail.com>
2022-09-21 11:54:46 +0200

    adiener

--- src/components/Home/CardProfiloHome.jsx
+++ src/components/Home/CardProfiloHome.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Col, Button,  } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
 
 const CardProfiloHome = () => {
     const user = useSelector(state => state.user.user)
     return (
         <Col className='CardProfile'>
-            <Col className="CardProfileImages" xs={12} >
-
-                <Col xs={3} className='CardProfileCerchio d-flex align-items-end'>
-                    <img className="img-fluid CardProfileCerchioImg" src={user.image} alt="" />
+            <Col className="CardProfileImages d-flex justify-content-center mb-5" xs={12} >
+                <Col xs={3} className='HomeCardProfileCerchio d-flex align-items-end'>
+                    <img className="img-fluid HomeCardImg" src={user.image} alt="" />
                 </Col>
             </Col>
             <Col xs={12} className='CardProfileText'>
@@ -18,14 +17,10 @@ const CardProfiloHome = () => {
                 <p className="text-secondary">{user.area}{' '}<span className="text-primary">informazioni di contatto</span> </p>
             </Col>
             <Col xs={12} className='d-flex justify-content-start p-4'>
-                <Col className="CardProfileDisp" xs={8}>
-                    <h6 className="m-0">Disponibile a lavorare</h6>
-                    <p className="m-0">Ruoli di {user.title}</p>
-                    <p className="text-primary m-0">Vedi informazioni di contatto</p>
-                </Col>
+
             </Col>
         </Col>
-  )
+    )
 }
 
 export default CardProfiloHome
