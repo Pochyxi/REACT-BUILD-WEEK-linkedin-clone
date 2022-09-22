b29d358d3eff0d5d0c19ddda89f090b5f8193fc2
Pochyxi <adienerlopez@gmail.com>
2022-09-22 11:54:55 +0200

    Update FormDialogPost.jsx

--- src/components/Home/FormDialogPost.jsx
+++ src/components/Home/FormDialogPost.jsx
@@ -1,4 +1,4 @@
-import Button from '@mui/material/Button';
+import { Button, Col } from 'react-bootstrap';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -7,10 +7,17 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
+import FeedIcon from '@mui/icons-material/Feed';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import BarChartIcon from '@mui/icons-material/BarChart';
 
 
 export default function FormDialogPost({ fetchPosts }) {
     const token = useSelector(state => state.user.token)
+    const user = useSelector(state => state.user.user)
     const [open, setOpen] = useState(false);
     const [formObj, setFormObj] = useState({
         text: '',
@@ -62,16 +69,6 @@ export default function FormDialogPost({ fetchPosts }) {
         return validation
     }
 
-    // const handleExperience = (key, value) => {
-    //     setExperienceObj(form => {
-    //         return {
-    //             ...form,
-    //             [key]: value
-    //         }
-
-    //     })
-    // }
-
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -82,41 +79,76 @@ export default function FormDialogPost({ fetchPosts }) {
 
     return (
         <>
-            <Button onClick={handleClickOpen} variant="outline-secondary" className="buttonPost" >
-                Crea Post
+            <Button
+                onClick={handleClickOpen}
+                variant="outline-secondary"
+                className="buttonPost"
+            >
+                Avvia un Post
             </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}>
-                <DialogTitle>COMPETENZE</DialogTitle>
+            <Dialog fullWidth={"lg"} open={open} onClose={handleClose}>
+                <Col className="d-flex justify-content-between align-items-center">
+                    <DialogTitle>Crea un post</DialogTitle>
+                    <Button
+                        className="buttonDialogPost"
+                        variant="outline-secondary "
+                        onClick={handleClose}
+                    >
+                        X
+                    </Button>
+                </Col>
+                <hr />
                 <DialogContent>
-                    <DialogContentText>
-                        Raccontaci le tue esperienze lavorative
-                    </DialogContentText>
+                    <Col xs={12} className="d-flex justify-content-start">
+                        <img className="NavbarUserList" src={user.image} alt="" />
+                        <div className="mb-3 ">
+                            <h6>
+                                {user.name} {user.surname}
+                            </h6>
+                            <Button className="CardAziendaButton" variant="outline-secondary">
+                                Chiunque
+                            </Button>
+                        </div>
+                    </Col>
                     <TextField
                         autoFocus
-                        className='mt-3'
+                        className="mt-3"
                         id="outlined-multiline-flexible"
-                        label="Mansione svolta"
+                        label="Di cosa vorresti parlare"
                         multiline
                         rows={5}
                         value={formObj.text}
-                        onChange={(e) => { handleForm('text', e.target.value); }}
+                        onChange={(e) => {
+                            handleForm("text", e.target.value);
+                        }}
                         fullWidth
                         variant="standard"
                         required
                     />
-
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>ANNULLA</Button>
-                    <Button onClick={() => {
-                        if (formValidation() === true) {
-                            handleClose()
-                            addPost(formObj)
-                        }
-
-                    }}>CONFERMA</Button>
+                    <Col xs={10} className="d-flex justify-content-around">
+                        <ImageOutlinedIcon sx={{ color: '#86888A' }} />
+                        <SmartDisplayIcon sx={{ color: '#86888A' }} />
+                        <FeedIcon sx={{ color: '#86888A' }} />
+                        <BusinessCenterIcon sx={{ color: '#86888A' }} />
+                        <WorkspacePremiumIcon sx={{ color: '#86888A' }} />
+                        <BarChartIcon sx={{ color: '#86888A' }} />
+                    </Col>
+                    <Col xs={2} >
+                        <Button
+                            className="CardPostButton"
+                            variant="outline-secondary"
+                            onClick={() => {
+                                if (formValidation() === true) {
+                                    handleClose();
+                                    addPost(formObj);
+                                }
+                            }}
+                        >
+                            Pubblica
+                        </Button>
+                    </Col>
                 </DialogActions>
             </Dialog>
         </>
