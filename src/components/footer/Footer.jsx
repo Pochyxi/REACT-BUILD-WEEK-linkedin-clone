d3f8f72a136cbbc9c2ce392c12938e0dc282809e
Pochyxi <adienerlopez@gmail.com>
2022-10-01 16:14:02 +0200

    footer work

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import './Footer.css';
-import { Col , Container , Row } from "react-bootstrap";
+import { Col , Container , Row, Form, Dropdown } from "react-bootstrap";
+import { GrLinkedin } from "react-icons/gr"
+import { FaQuestionCircle } from "react-icons/fa"
+import { AiTwotoneSetting } from "react-icons/ai"
+import { IoIosArrowDown } from "react-icons/io";
 
 
 
 
 const Footer = () => {
     return (
-        <Container fluid>
+        <Container fluid className={"text-secondary"}>
             <Row className={''}>
-                <div className='titleLink'>
-                    <h4>LINKEDIN</h4>
+                <div className='titleLink d-flex align-items-center'>
+                    <p className={"m-0 p-0 "}>Linked</p><GrLinkedin className={"inIcon"} />
                 </div>
             </Row>
-            <Row className={"d-flex flex-column flex-sm-row justify-content-between"}>
+            <Row className={"footerSecondaRow d-flex flex-column flex-sm-row justify-content-between"}>
                 <Col xs={12} md={6} className={"d-flex flex-wrap justify-content-between"} >
-                    <Col>
+                    <Col className={"m-1"}>
                         <p>Informazioni</p>
                         <p>Talent solutions</p>
                         <p>Carriera</p>
-                        <p>Privacy e condizioni</p>
+                        <p>
+                            <Dropdown >
+                                <Dropdown.Toggle variant="success" id="dropdown-basic" className={"footerDrop"}>
+                                    Dropdown Button <IoIosArrowDown />
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu className={"text-center"}>
+                                    <Dropdown.Item className={"footerDropMenu"} href="#/action-1">Informativa sulla privacy</Dropdown.Item>
+                                    <Dropdown.Item className={"footerDropMenu"} href="#/action-2">Contratto di licenza</Dropdown.Item>
+                                    <Dropdown.Item className={"footerDropMenu"} href="#/action-3">Informativa sui cookie</Dropdown.Item>
+                                    <Dropdown.Item className={"footerDropMenu"} href="#/action-3">Informativa sui copyright</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </p>
+
                         <p>Pubblicità</p>
                         <p>Mobile</p>
                         <p>Centro sicurezza</p>
@@ -37,13 +54,53 @@ const Footer = () => {
                         </Col>
                     </Col>
                 </Col>
-                <Col xs={12} sm={6} className={"d-flex flex-column ps-5"}>
-                        <p>Domande?</p>
-                        <p>Visita il nostro centro assistenza</p>
-                        <p>Gestisci il tuo account e la tua privacy</p>
-                        <p>Vai alle impostazioni</p>
+                <Col xs={12} sm={6} className={"d-flex flex-column ps-3 ps-md-5 align-items-start"}>
+                    <Col xs={12}>
+                        <Col xs={12} className={"d-flex align-items-start"}>
+                            <Col xs={1} className={"text-center pt-1 pe-1"}>
+                                <FaQuestionCircle className={"footerIconDomande"}/>
+                            </Col>
+
+                            <Col xs={9}>
+                                <p className={"m-0 footerDomande"}>Domande?</p>
+                                <p className={"footerTestoSecondario"}>Visita il nostro centro assistenza</p>
+                            </Col>
+                        </Col>
+                        <Col xs={12} className={"d-flex align-items-start"}>
+                            <Col xs={1} className={"text-center pt-1 pe-1"}>
+                                <AiTwotoneSetting className={"footerIconGestisci"}/>
+                            </Col>
+                            <Col xs={9}>
+                                <p className={"m-0 footerGestisci"}>Gestisci il tuo account e la tua privacy</p>
+                                <p className={"footerTestoSecondario"}>Vai alle impostazioni</p>
+                            </Col>
+                        </Col>
+                    </Col>
+                    <Col xs={12} >
+                        <p className={""}>Seleziona lingua</p>
+                        <Col xs={12} className={"d-none d-md-block"}>
+                            <Form.Select >
+                                <option>Italiano (Italiano)</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </Col>
+                    </Col>
+                </Col>
+                <Col xs={12} className={"d-md-none"}>
+                    <p >Seleziona lingua</p>
+                    <Col xs={12}>
+                        <Form.Select >
+                            <option>Italiano (Italiano)</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
+                    </Col>
                 </Col>
             </Row>
+
             <small>Linkedin corporation &copy; 2022</small>
         </Container>
 
