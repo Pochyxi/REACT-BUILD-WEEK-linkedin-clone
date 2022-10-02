39ca50639ec1031e24cf3873ce1f910d042ba315
Pochyxi <adienerlopez@gmail.com>
2022-10-02 12:57:22 +0200

    footer work

--- src/index.js
+++ src/index.js
@@ -6,18 +6,12 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store , persistor } from "./redux/store/index";
 import { PersistGate } from "redux-persist/integration/react";
-import { DevSupport } from "@react-buddy/ide-toolbox";
-import { ComponentPreviews , useInitial } from "./dev";
 
 const root = ReactDOM.createRoot ( document.getElementById ( "root" ) );
 root.render (
     <Provider store={ store }>
         <PersistGate persistor={ persistor }>
-            <DevSupport ComponentPreviews={ ComponentPreviews }
-                        useInitialHook={ useInitial }
-            >
                 <App/>
-            </DevSupport>
         </PersistGate>
     </Provider>
 );
